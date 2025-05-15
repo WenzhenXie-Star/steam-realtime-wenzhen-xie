6168fe41bb2d0a5a2f14f90bfa17cf3957a0ca1d
解文振 <3452089388@qq.com>
2025-05-15 22:58:49 +0800

    20250515

---
+++ realtime_dev3/src/main/java/com/xwz/bean/DimCategoryCompare.java
@@ -0,0 +1,20 @@
+package com.xwz.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Package com.xwz.retail.v2.reaktime.bean.DimCategoryCompare
+ * @Author Wenzhen.Xie
+ * @Date 2025/5/14 22:23
+ * @description: DimCategoryCompare
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class DimCategoryCompare {
+    private Integer id;
+    private String categoryName;
+    private String searchCategory;
+}
